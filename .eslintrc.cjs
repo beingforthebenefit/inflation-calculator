7a3875ef0af7e54ce5df95c7b08706b2583dc354
Gerald Todd <beingforthebenefit@gmail.com>
2024-01-30 21:20:33 -0800

    Install and configure eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,21 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': ['error', {'endOfLine': 'auto'}],
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    'object-curly-spacing': ['error', 'never'],
+    'react/jsx-curly-spacing': [2, {'when': 'never', 'children': true}]
   },
 }
